3f1fb534731773032ca11f0e4051084f2d322f73
vishwesh129 <nakkalavishwesh@gmail.com>
2023-03-15 21:35:26 +0530

    landing page

--- index.js
+++ index.js
@@ -25,3 +25,33 @@ function slideshowOffer() {
 }
 
 slideshowOffer();
+
+
+function slideshowBestSellers() {
+    let slide_img = document.getElementById('slide-img');
+    let img = document.createElement('img');
+
+    let best_sellers_arr = ['https://cdn.shopify.com/s/files/1/0283/0185/2747/files/vitablast-c-education-hero-des_2000x.jpg?v=1677862803',
+        'https://cdn.shopify.com/s/files/1/0283/0185/2747/files/magnesium-candles-hp-hero-hero_2000x.jpg?v=1677862298', 
+        'https://cdn.shopify.com/s/files/1/0283/0185/2747/files/eyes-routine-hero-des_2000x.jpg?v=1677796767', 
+        'https://cdn.shopify.com/s/files/1/0283/0185/2747/files/spring-makeup-edit-hero-des_2000x.jpg?v=1677796413'
+    ];
+
+    img.src = best_sellers_arr[0];
+    img.style.width = '100%';
+    slide_img.append(img);
+    let i = 1;
+    setInterval(function () {
+        img.src = best_sellers_arr[i % best_sellers_arr.length];
+        img.style.width = '100%';
+        i++;
+        slide_img.append(img);
+    }, 3000);
+
+
+    let dots = document.getElementById('dots-slide');
+    let dot_div = document.createElement('div');
+    dot_div.style.display = 'inline-block';
+}
+
+slideshowBestSellers();
