68c22170896eb253d8cb263ede6b405a8fd0203e
arpit017 <shuklaji9706@gmail.com>
2023-03-20 19:27:18 +0530

    updated

---
+++ scripts/login.js
@@ -0,0 +1,24 @@
+var credentialsdata=JSON.parse(localStorage.getItem("credentials"))||[]
+document.querySelector("form").addEventListener("submit",checkdata)
+function checkdata(){
+ event.preventDefault();
+ var f=0
+ var email=document.querySelector("#email").value
+ var password=document.querySelector("#password").value
+ 
+ credentialsdata.map(function(ele){
+     if(email==ele.email && password==ele.password){
+        document.getElementById("showUsername").textContent = "Logged in as  "+ele.firstname+" ";
+         window.location.assign("hompage.html");
+         f=1
+         localStorage.setItem("username",ele.firstname);
+     }
+ })
+   if(f==0){
+     document.querySelector("#wrongPass").textContent=" X Ooops!  Wrong Credentials"
+   }      
+     
+   document.getElementById("email").value = null;
+   document.getElementById("password").value = null;
+    
+}
