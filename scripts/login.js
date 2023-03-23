2f499de05dad09084dea33ce8201e521adf80006
RahulDobariya <sendydobariya007@gmail.com>
2023-03-23 12:34:36 +0530

    day 2 product constructor

--- scripts/login.js
+++ scripts/login.js
@@ -1,24 +1,24 @@
-var credentialsdata=JSON.parse(localStorage.getItem("credentials"))||[]
-document.querySelector("form").addEventListener("submit",checkdata)
-function checkdata(){
- event.preventDefault();
- var f=0
- var email=document.querySelector("#email").value
- var password=document.querySelector("#password").value
- 
- credentialsdata.map(function(ele){
-     if(email==ele.email && password==ele.password){
-        document.getElementById("showUsername").textContent = "Logged in as  "+ele.firstname+" ";
-         window.location.assign("../html/index.html");
-         f=1
-         localStorage.setItem("username",ele.firstname);
-     }
- })
-   if(f==0){
-     document.querySelector("#wrongPass").textContent=" X Ooops!  Wrong Credentials"
-   }      
-     
-   document.getElementById("email").value = null;
-   document.getElementById("password").value = null;
-    
+var credentialsdata = JSON.parse(localStorage.getItem("credentials")) || []
+document.querySelector("form").addEventListener("submit", checkdata)
+function checkdata() {
+  event.preventDefault();
+  var f = 0
+  var email = document.querySelector("#email").value
+  var password = document.querySelector("#password").value
+
+  credentialsdata.map(function (ele) {
+    if (email == ele.email && password == ele.password) {
+      document.getElementById("showUsername").textContent = "Logged in as  " + ele.firstname + " ";
+      window.location.assign("index.html");
+      f = 1
+      localStorage.setItem("username", ele.firstname);
+    }
+  })
+  if (f == 0) {
+    document.querySelector("#wrongPass").textContent = " X Ooops!  Wrong Credentials"
+  }
+
+  document.getElementById("email").value = null;
+  document.getElementById("password").value = null;
+
 }
